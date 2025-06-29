bd909283d93f53ca3a4ce497091b453093a06d19
AntonioHollerman <toniohollerProf@gmail.com>
2025-06-28 21:45:20 -0400

    implement temperature

--- GameDev/Assets/Scripts/Managers/TemperatureManager.cs
+++ GameDev/Assets/Scripts/Managers/TemperatureManager.cs
@@ -1,17 +1,36 @@
 using System;
 using AtomBehaviour;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Managers
 {
     public class TemperatureManager : MonoBehaviour
     {
-        public float f;
+        public TextMeshProUGUI tempDisplay;
+        public Slider tempSlider;
+        public float tempChangePerSecond;
+        
+        public static float K = 300f;
+        
+        
         private void Update()
         {
-            Atom.SpeedMultiplier = (1 / 57f) * (f - 0.5f) - 273;
-            Atom.ChanceToBreak = (30 / 57f) * (f - 10) - 273;
-            Atom.ChanceToBond = (40 / 57f) * (f - 50) - 273;
+            tempDisplay.text = $"Temp: {(int) K}K";
+            Atom.SpeedMultiplier = (1 / 57f) * (K - 273) + 0.5f;
+            Atom.ChanceToBreak = (30 / 57f) * (K - 273) + 10;
+            Atom.ChanceToBond = (40 / 57f) * (K - 273) + 50;
+
+            if (Mathf.Approximately(tempSlider.value, 1))
+            {
+                K = Mathf.Clamp(K - tempChangePerSecond * Time.deltaTime, 273, 330);
+            }
+
+            if (Mathf.Approximately(tempSlider.value, 3))
+            {
+                K = Mathf.Clamp(K + tempChangePerSecond * Time.deltaTime, 273, 330);
+            }
         }
     }
 }
